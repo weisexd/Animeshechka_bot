6cba5d0c137ceab8d7557ec0facd424cb2ebad00
weisexd <qwe>
2025-08-14 14:31:49 +0300

    Add help telegram command

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -1,8 +1,10 @@
 import { Telegraf } from 'telegraf';
 import ping from './ping';
 import initializeRandomWordsCommands from '../features/randomWords/commands';
+import help from './help';
 
 export default function initializeCommands(bot: Telegraf): void {
     ping(bot);
     initializeRandomWordsCommands(bot);
+    help(bot);
 }
