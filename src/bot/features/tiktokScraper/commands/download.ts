fdf6cd3660c455921e224b9a249239d86baf9b28
weisexd <qwe>
2026-01-03 09:02:48 +0300

    Add empty download command

---
+++ src/bot/features/tiktokScraper/commands/download.ts
@@ -0,0 +1,9 @@
+import { BotCommand } from '../../../types/BotCommand';
+
+const download: BotCommand = (bot) => {
+    bot.command('download', (ctx) => {
+        
+    });
+};
+
+export default download;
